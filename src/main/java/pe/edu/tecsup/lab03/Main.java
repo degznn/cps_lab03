b003104c92882f5b39061493151b848884e43241
degznn <diego.condori.m@tecsup.edu.pe>
2026-09-05 16:57:09 -0500

    Primer commit del laboratorio 03

---
+++ src/main/java/pe/edu/tecsup/lab03/Main.java
@@ -0,0 +1,11 @@
+package pe.edu.tecsup.lab03;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Laboratorio 03 - Control de Versiones Avanzadas");
+
+    }
+
+}
